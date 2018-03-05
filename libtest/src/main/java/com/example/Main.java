3c5928bdf1597163000793bb50cc7b3518c385ef
Mel <melytedy_1@hotmail.com>
2018-03-05 02:46:18 -0300

    Parte 1 Basic of the Basic

--- libtest/src/main/java/com/example/Main.java
+++ libtest/src/main/java/com/example/Main.java
@@ -1,4 +1,33 @@
 package com.example;
 
 public class Main {
-}
+
+    public static void main(String arg[])
+    {
+        //Escribiendo mis primeras lineas XD (Tutorial EASY PLZ XD)
+        System.out.println("HOLA MUNDO ANDROID ATC");
+        System.out.println("Java Fundamentals for Android™ Development");
+        System.out.println("Android™ Application Development");
+        System.out.println("Android™ Security Essentials");
+        System.out.println("Monetize Android™ Applications");
+        System.out.println("For more information check www.androidatc.com");
+        /*
+         Pucha que es fome esta cosa si ya sabes.
+         Todo sea por aprender android.
+         */
+
+        //Vienen los numeritos
+        System.out.println("TRABAJO CON VARIABLES");
+        int a=1;
+        int b=2;
+        //la pequeña suma de siempre
+        int c= a+b;
+
+        System.out.println("La suma entre 1+2 es = "+c);
+        System.out.println("Demostracion de variable 'boolean'");
+        //HERE COMES ANOTHER LESSON
+        boolean verdadero=true;
+        boolean falso=false;
+        System.out.println("La verdad es: "+verdadero+" y lo falso: "+falso);
+
+    }}
